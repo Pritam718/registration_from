9539043908623c078f25c3ab4ea061e780b259db
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2023-11-27 22:38:39 +0530

    proxy

--- src/Config/ap.config.js
+++ src/Config/ap.config.js
@@ -1,10 +1,6 @@
 const isProd = true;
-export const BASE_URL = isProd
-  ? "https://student-enrolment-backend.onrender.com/api"
-  : "http://localhost:8000/api";
-export const SOCKET_URL = isProd
-  ? "https://student-enrolment-backend.onrender.com/"
-  : "http://localhost:8000";
+export const BASE_URL = isProd ? "/api" : "http://localhost:8000/api";
+export const SOCKET_URL = isProd ? "" : "http://localhost:8000";
 
 //student url
 export const STUDENT_URL = `${BASE_URL}/student`;
