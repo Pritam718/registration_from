619796b19166070f850b93830cdd0248b5d4f191
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2023-11-27 22:44:12 +0530

    stable for local

--- src/Config/ap.config.js
+++ src/Config/ap.config.js
@@ -1,6 +1,10 @@
-const isProd = true;
-export const BASE_URL = isProd ? "/api" : "http://localhost:8000/api";
-export const SOCKET_URL = isProd ? "" : "http://localhost:8000";
+const isProd = false;
+export const BASE_URL = isProd
+  ? "https://student-enrolment-backend.onrender.com/api"
+  : "http://localhost:8000/api";
+export const SOCKET_URL = isProd
+  ? "https://student-enrolment-backend.onrender.com/"
+  : "http://localhost:8000";
 
 //student url
 export const STUDENT_URL = `${BASE_URL}/student`;
