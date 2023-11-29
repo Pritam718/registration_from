95ceee654e897dbd6a7db789226841711c589f44
Pritam718 <pritam762.hitcsecs2020@gmail.com>
2023-11-29 17:24:25 +0530

    dark mood fixed

--- src/Pages/Student.jsx
+++ src/Pages/Student.jsx
@@ -32,14 +32,14 @@ function Student() {
                 <div>
                   <label
                     htmlFor="first_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     First name
                   </label>
                   <input
                     type="text"
                     id="first_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     placeholder="First Name"
                     {...register("fname", { required: true })}
                   />
@@ -47,14 +47,14 @@ function Student() {
                 <div>
                   <label
                     htmlFor="last_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Last name
                   </label>
                   <input
                     type="text"
                     id="last_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     placeholder="Last Name"
                     {...register("lname", { required: true })}
                   />
@@ -63,14 +63,14 @@ function Student() {
                 <div>
                   <label
                     htmlFor="phone"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Phone number
                   </label>
                   <input
                     type="tel"
                     id="phone"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     placeholder="Phone Number"
                     {...register("phone", { required: true })}
                   />
@@ -78,14 +78,14 @@ function Student() {
                 <div>
                   <label
                     htmlFor="website"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 "
                   >
                     Website URL
                   </label>
                   <input
                     type="url"
                     id="website"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     placeholder="Website Link"
                     {...register("website", { required: true })}
                   />
@@ -94,70 +94,70 @@ function Student() {
               <div className="mb-6">
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Email Address
                 </label>
                 <input
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
                   placeholder="Email Address"
                   {...register("email", { required: true })}
                 />
               </div>
 
               <div>
-                <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+                <h3 className="mb-4 font-semibold text-gray-900 ">
                   Gender
                 </h3>
-                <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex ">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="horizontal-list-radio-license"
                         type="radio"
                         value="male"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
                         {...register("gender")}
                       />
                       <label
                         htmlFor="horizontal-list-radio-license"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         Male
                       </label>
                     </div>
                   </li>
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="horizontal-list-radio-id"
                         type="radio"
                         value="female"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
                         {...register("gender")}
                       />
                       <label
                         htmlFor="horizontal-list-radio-id"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         Female
                       </label>
                     </div>
                   </li>
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="horizontal-list-radio-millitary"
                         type="radio"
                         value="other"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 "
                         {...register("gender")}
                       />
                       <label
                         htmlFor="horizontal-list-radio-millitary"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         Other
                       </label>
@@ -166,56 +166,56 @@ function Student() {
                 </ul>
               </div>
               <div>
-                <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+                <h3 className="mb-4 font-semibold text-gray-900 ">
                   Skills
                 </h3>
-                <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex ">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="vue-checkbox-list"
                         type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 "
                         value={true}
                         {...register("java")}
                       />
                       <label
                         htmlFor="vue-checkbox-list"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         Java
                       </label>
                     </div>
                   </li>
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="react-checkbox-list"
                         type="checkbox"
                         value={true}
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 "
                         {...register("html")}
                       />
                       <label
                         htmlFor="react-checkbox-list"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         HTML
                       </label>
                     </div>
                   </li>
-                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                  <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r ">
                     <div className="flex items-center pl-3">
                       <input
                         id="angular-checkbox-list"
                         type="checkbox"
                         value={true}
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 "
                         {...register("css")}
                       />
                       <label
                         htmlFor="angular-checkbox-list"
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 "
                       >
                         CSS
                       </label>
@@ -226,7 +226,7 @@ function Student() {
 
               <button
                 type="submit"
-                className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
               >
                 Submit
               </button>
