d4f6025774d825447ec8b76ad18c9240906f12ec
Pritam718 <pritam762.hitcsecs2020@gmail.com>
2023-11-29 16:49:55 +0530

    updateModal small change

--- src/Components/UpdateModalComponent.jsx
+++ src/Components/UpdateModalComponent.jsx
@@ -61,7 +61,7 @@ const UpdateModalComponent = ({
               id="first_name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="First Name"
-              value={fname}
+              
               {...register("fname", { required: true })}
             />
             <br />
@@ -70,7 +70,7 @@ const UpdateModalComponent = ({
               id="last_name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Last Name"
-              value={lname}
+              
               {...register("lname", { required: true })}
             />
             <br />
